066d3002cecb49377d55f2f1613e8893717d8ccd
Maya the bee <hello@unw.re>
2025-12-20 16:52:57 +0100

    lib: pub the Error type

--- src/engine/error.rs
+++ src/engine/error.rs
@@ -6,12 +6,15 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 /// An error that may occur when interracting with the engine.
 #[derive(Debug, Error)]
 pub enum Error {
+    /// An I/O error occured.
     #[error("I/O error: {0}")]
     Io(#[from] std::io::Error),
 
+    /// An error occured while (de)-serializing messages.
     #[error("format error: {0}")]
     Format(#[from] crate::format::Error),
 
+    /// The data stream was closed before expected.
     #[error("got an unexpected end of stream from engine")]
     UnexpectedEof,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,4 @@ pub mod format;
 mod subable;
 
 mod engine;
-pub use engine::Engine;
+pub use engine::{Engine, Error};
